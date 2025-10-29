00bcd812cafb73d0f200bff8b17814a70eb508f7
christophernadalin123 <christopher.nadalin@escola.pr.gov.br>
2025-10-29 20:00:23 -0300

    Create lacos-repeticao.py

---
+++ lacos-repeticao.py
@@ -0,0 +1,25 @@
+# 1. Contagem do tempo até o fim de semana (Loop for)
+dias = ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"]
+
+for dia in dias:
+    print("Contando... hoje é", dia)
+
+print("Finalmente chegou o fim de semana!")
+
+
+
+
+# 4. Cálculo do total da lista de compras (Loop for)
+precos = [5.50, 3.20, 8.90, 2.50, 10.00]
+total = 0
+
+for preco in precos:
+    total += preco
+
+print("Total da compra: R$", total)
+
+
+
+
+# 7. Leitura de mensagens não lidas (Loop while)
+mensagens_n_
