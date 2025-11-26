6233e2201705a3f436b8fad45007c26631c31ec8
christophernadalin123 <christopher.nadalin@escola.pr.gov.br>
2025-11-25 23:09:31 -0300

    Create calculaPreco.cs

---
+++ calculaPreco.cs
@@ -0,0 +1,49 @@
+
+using System;
+
+class Produto
+
+{
+
+    public string Nome { get; set; }
+
+    public double Preco { get; set; }
+
+    public double Desconto { get; set; }
+
+
+    public double CalcularPrecoFinal()
+
+    {
+
+        return Preco - (Preco * Desconto / 100);
+
+    }
+
+}
+
+class Program
+
+{
+
+    static void Main()
+
+    {
+
+        Produto produto = new Produto
+
+        {
+
+            Nome = "Fone de ouvido",
+
+            Preco = 200,
+
+            Desconto = 25
+
+        };
+
+        Console.WriteLine($"Preço final de {produto.Nome}: R$ {produto.CalcularPrecoFinal():F2}");
+
+    }
+
+}
