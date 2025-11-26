aea291ed2b9ca96f847585f86cadf8bff13add13
christophernadalin123 <christopher.nadalin@escola.pr.gov.br>
2025-11-25 23:07:09 -0300

    Create calculaPreco.js

---
+++ calculaPreco.js
@@ -0,0 +1,23 @@
+function calcularPrecoFinal(produto) {
+  const desconto = produto.desconto ?? 0;
+
+  const precoFinal = produto.preco - (produto.preco * desconto / 100);
+
+  console.log(`Preço final de ${produto.nome}: R$ ${precoFinal.toFixed(2)}`);
+
+}
+
+// Exemplo de uso
+
+const produto1 = {
+
+  nome: "Fone de ouvido",
+
+  preco: 200,
+
+  desconto: 25
+
+};
+
+calcularPrecoFinal(produto1);
+
